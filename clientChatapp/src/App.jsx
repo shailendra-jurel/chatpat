f077d8f99beda3d0a441400d5c608324c635b509
Shailendra Jurel <shailendrajurel001@gmail.com>
2025-02-12 01:27:31 +0000

    initial commit

---
+++ clientChatapp/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <> 
+   <h1>Hello this is simple client app based application for chatting</h1>
+   <h2> how are you heu hero </h2>
+   </>
+  )
+}
+
+export default App
